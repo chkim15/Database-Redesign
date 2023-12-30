38b99ea6fc023c6013de567886addfb9f8ccd784
Chulhee Kim <80579544+chkim15@users.noreply.github.com>
2023-12-30 00:28:54 -0500

    Update Database Redesign.sql

--- Database Redesign.sql
+++ Database Redesign.sql
@@ -3,14 +3,14 @@ USE dankim_corporation;
 
 CREATE TABLE customers (
 
-  customer_id		INT NOT NULL AUTO_INCREMENT,
+  customer_id			INT NOT NULL AUTO_INCREMENT,
   name				VARCHAR(50) NOT NULL UNIQUE,
   address			VARCHAR(255),
-  city		    	VARCHAR(50) NOT NULL,
+  city		    		VARCHAR(50) NOT NULL,
   country			VARCHAR(50) NOT NULL,
   phone				VARCHAR(50) UNIQUE,
   email				VARCHAR(100) UNIQUE,
-  PRIMARY KEY		(customer_id)
+  PRIMARY KEY			(customer_id)
 );
 
 CREATE TABLE items (
@@ -21,47 +21,47 @@ CREATE TABLE items (
   width				INT,
   weight			INT,
   hs_code			INT,
-  PRIMARY KEY		(item_id)
+  PRIMARY KEY			(item_id)
 );
 
 CREATE TABLE orders (
   
   order_id			INT NOT NULL AUTO_INCREMENT,
-  customer_id		INT NOT NULL,
+  customer_id			INT NOT NULL,
   item_id			INT NOT NULL,
   export_id			INT NOT NULL,
-  import_id  		INT,
+  import_id  			INT,
   quantity			INT NOT NULL,
   price				DECIMAL(15,2) NOT NULL,
-  PRIMARY KEY		(order_id),
-  CONSTRAINT		customers
-    FOREIGN KEY 	(customer_id)
-    REFERENCES		customers(customer_id)
+  PRIMARY KEY			(order_id),
+  CONSTRAINT			customers
+    FOREIGN KEY 		(customer_id)
+    REFERENCES			customers(customer_id)
     ON DELETE CASCADE,
-  CONSTRAINT		items
-    FOREIGN KEY		(item_id)
-    REFERENCES		items(item_id)
+  CONSTRAINT			items
+    FOREIGN KEY			(item_id)
+    REFERENCES			items(item_id)
     ON DELETE CASCADE,
-  CONSTRAINT		exports
-    FOREIGN KEY		(export_id)
-    REFERENCES		exports(export_id)
+  CONSTRAINT			exports
+    FOREIGN KEY			(export_id)
+    REFERENCES			exports(export_id)
     ON DELETE CASCADE,
-  CONSTRAINT		imports
-    FOREIGN KEY		(import_id)
-    REFERENCES		imports(import_id)
+  CONSTRAINT			imports
+    FOREIGN KEY			(import_id)
+    REFERENCES			imports(import_id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE payment (
 
-  payment_id 		INT NOT NULL AUTO_INCREMENT,
+  payment_id 			INT NOT NULL AUTO_INCREMENT,
   order_id			INT NOT NULL,
-  remittance 		DECIMAL(15,2) NOT NULL,
+  remittance 			DECIMAL(15,2) NOT NULL,
   date				DATE NOT NULL,
-  PRIMARY KEY		(payment_id),
-  CONSTRAINT		orders
-    FOREIGN KEY 	(order_id)
-    REFERENCES		orders(order_id)
+  PRIMARY KEY			(payment_id),
+  CONSTRAINT			orders
+    FOREIGN KEY 		(order_id)
+    REFERENCES			orders(order_id)
     ON DELETE CASCADE
 );
 
@@ -69,9 +69,9 @@ CREATE TABLE exports (
 
   export_id			INT NOT NULL AUTO_INCREMENT,
   date				DATE,
-  destination		VARCHAR(50) NOT NULL,
+  destination			VARCHAR(50) NOT NULL,
   freight			INT NOT NULL,
-  PRIMARY KEY		(export_id)
+  PRIMARY KEY			(export_id)
 );
 
 CREATE TABLE imports (
@@ -79,20 +79,20 @@ CREATE TABLE imports (
   import_id			INT NOT NULL AUTO_INCREMENT,
   arrival			VARCHAR(50) NOT NULL,
   freight			INT NOT NULL,
-  PRIMARY KEY		(import_id)
+  PRIMARY KEY			(import_id)
 );
 
 CREATE TABLE production (
 
-  production_id		INT NOT NULL AUTO_INCREMENT,
+  production_id			INT NOT NULL AUTO_INCREMENT,
   order_id			INT NOT NULL,
-  factory_name		VARCHAR(50) NOT NULL,
+  factory_name			VARCHAR(50) NOT NULL,
   quantity			INT NOT NULL,
   price				INT NOT NULL,
-  PRIMARY KEY		(production_id),
-  CONSTRAINT		production_orders
-    FOREIGN KEY 	(order_id)
-    REFERENCES		orders(order_id)
+  PRIMARY KEY			(production_id),
+  CONSTRAINT			production_orders
+    FOREIGN KEY 		(order_id)
+    REFERENCES			orders(order_id)
     ON DELETE CASCADE
 );
 
@@ -100,7 +100,7 @@ CREATE TABLE exchanges (
 
   date				DATE NOT NULL UNIQUE,
   rate				DECIMAL(6,2) NOT NULL,
-  PRIMARY KEY 		(date)
+  PRIMARY KEY 			(date)
 );
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/exchange_rate.csv'
